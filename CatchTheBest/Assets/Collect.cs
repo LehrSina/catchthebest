5dec170c033253819d8a1e0f43a387119854e957
LehrSina <40425777+LehrSina@users.noreply.github.com>
2019-01-11 15:22:43 +0100

    Score erstellt (Canvas) und dazugehörige ScriptE (ScoreSystem + Collect). Dazuzählen funktioniert noch nicht richtig..

---
+++ CatchTheBest/Assets/Collect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour
+{
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        ScoreSystem.theScore += 50;
+        Destroy(gameObject);
+    }
+}
---
+++ CatchTheBest/Assets/ScoreSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public static int theScore;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        scoreText.GetComponent<Text>().text = "Score: " + theScore;
+    }
+
+}
