8f77c1c481e7cd55f04b422b8d8a88402a71dc9e
LehrSina <40425777+LehrSina@users.noreply.github.com>
2019-01-11 14:00:08 +0100

    Erste Objekte als Platzhalter erstellt (Welt, Player, Hinderniss) und Scripte für Camera, Player und Kollison mit Hinderniss geschrieben.

---
+++ CatchTheBest/Assets/Script/collision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Hinderniss")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
+
+
---
+++ CatchTheBest/Assets/Script/playerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void Start()
+    {
+        moveSpeed = 8f;
+    }
+
+    void Update()
+    {
+        //moveing with Arrow Keys
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical") * Time.deltaTime);
+
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (transform.position.y <= 1.05f)
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * 250.0f);
+            }
+        }
+    }
+}
+
+
+
+
+    
